aea19d49d9ff34f012d16a76ddc1ab7351e8f7cb
Ben Murphy <benmmurphy@gmail.com>
2012-02-15 23:08:34 +0000

    node communication

--- Remoting.hs
+++ Remoting.hs
@@ -2,22 +2,28 @@ module Remoting where
 
 import Remote
 import Control.Monad
+import Control.Concurrent
 
 initialProcess :: String -> ProcessM ()
 initialProcess "MASTER" = do
-  forM_ [1..10] $ \i -> do
-    (sp, rp) <- newChannel
-    pid <- spawnLocal (sayHello rp)
-    sendChannel sp  (i :: Int)
+  say "master!"
+  peers <- getPeers
+  say $ "peers: " ++ (show peers)
+  let slaves = findPeerByRole peers "SLAVE"
+  forM_ slaves $ \slave -> do
+    Just slaveProc <- nameQuery slave "slave"
+    send slaveProc "ping"
+
+initialProcess "SLAVE" = do
+  say "slave!"
+  nameSet "slave"
+  ping <- expect
+  say $ "SLAVE GOT " ++ show (ping :: String)
 
 initialProcess role = error $ "Unknown role " ++ role
 
-
-sayHello :: ReceivePort Int -> ProcessM ()
-sayHello rp =  do 
-  pid <- getSelfPid
-  n <- receiveChannel rp
-  say $ "Hello World " ++ show n
-
 main :: IO()
-main = remoteInit (Just "config") [] initialProcess
+main = do 
+  forkIO $ remoteInit (Just "slave") [] initialProcess
+  threadDelay (1000 * 1000 * 1)
+  remoteInit (Just "config") [] initialProcess
